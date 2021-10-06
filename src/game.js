1393530f68c03021dcc808b44ba73c9a2f22026d
Aramayis Sargsyan <mayissargsyan1995@gmail.com>
2021-10-06 18:35:24 +0400

    Pixi: bunny task

--- src/game.js
+++ src/game.js
@@ -1,33 +1,62 @@
 import * as PIXI from 'pixi.js'
 
-export class Game extends PIXI.Application {
+export class Game extends PIXI.Application  {
     constructor() {
         super({
             width: window.innerWidth,
             height: window.innerHeight,
             backgroundColor: 0xc3c3c3
         })
+console.log(this.renderer.width);
+console.log(this);
+        
 
         document.body.appendChild(this.view)
+        
+        const texture = PIXI.Texture.from('assets/bunny.png')
+        const bunny = new PIXI.Sprite(texture);
 
-        const loader = new PIXI.Loader()
-        loader.add('bunny', 'assets/bunny.png').load((loader, resources) => {
-            const bunny = new PIXI.Sprite(resources.bunny.texture);
+        bunny.x = this.renderer.width * 0.5;
+        bunny.y = this.renderer.height * 0.5;
+        bunny.anchor.x = 0.5;
+        bunny.anchor.y = 0.5;
+
+        this.stage.addChild(bunny);
+            this.ticker.add(animate);
+            function animate()  {
+                        bunny.rotation += 0.1;
+                    }
+      
+        // const texture = PIXI.Texture.from('assets/bunny.png')
+        
+        // // loader.add('bunny', 'assets/bunny.png').load((loader, resources) => {
+        //     const bunny = new PIXI.Sprite(texture);
+        //     bunny.x = this.renderer.width * 0.5;
+        //     bunny.y = this.renderer.height * 0.5;
+
+        //     // bunny.anchor.x = 0.5;
+        //     // bunny.anchor.y = 0.5;
+        //     this.stage.addChild(bunny);
 
 
-            bunny.x = this.renderer.width * 0.5;
-            bunny.y = this.renderer.height * 0.5;
+        //     this.ticker.add(animate);
+        //     this.ticker.start();
 
-            bunny.anchor.x = 0.5;
-            bunny.anchor.y = 0.5;
+        //     ticker.add(()=>{
+        //         bunny.rotation += 0.1;
+                
+        //     });
+        //     //ticker.start();
 
+        //     // this.ticker.add(() => {
+        //     //     bunny.rotation += 0.04;
+        //     // });
 
-            this.stage.addChild(bunny);
-
-            this.ticker.add(() => {
-                bunny.rotation += 0.04;
-            });
-        })
+        //     function animate()  {
+        //         bunny.rotation += 0.1;
+        //         this.render(stage);
+        //     }
+        // // })
     }
 }
 
